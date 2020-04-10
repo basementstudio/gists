7738e735950466ad27b732e5761bea6508001efe
Nazareno <nazarenooviedo@gmail.com>
2020-04-10 12:40:10 -0300

    Create useSessionStorage.js

---
+++ hooks/useSessionStorage.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+export const useSessionStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.sessionStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.sessionStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      /* empty */
+    }
+  };
+
+  return [storedValue, setValue];
+};
