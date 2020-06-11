8bbc160a5be8093087ab1814a020f0daee42a2d5
arzafran <franco@basement.studio>
2020-06-11 11:09:47 -0300

    updates

--- settings/next.config.js
+++ settings/next.config.js
@@ -1,26 +1,15 @@
 const withSvgr = require("next-svgr")
 
-// yarn add next-svgr babel-loader babel-preset-react-app
+// yarn add next-svgr
 
 module.exports = withSvgr({
   target: "serverless",
-  experimental: {
-    jsconfigPaths: true,
-    polyfillsOptimization: true,
-    pages404: true,
-  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.js$/,
-      include: /node_modules[\\/]idb/,
-      use: {
-        loader: require.resolve("babel-loader"),
-        options: {
-          presets: [[require.resolve("babel-preset-react-app/dependencies")]],
-        },
-      },
+      include: /node_modules[\\/]idb/
     })
 
     return config
-  },
+  }
 })
