6cd573a3d697dbfdf43fda666efa89f9ddf9708c
Nazareno Oviedo <nazareno@basement.studio>
2020-10-14 11:46:41 -0300

    feat: add hook useEventListener

---
+++ hooks/useEventListener.js
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react';
+
+export const useEventListener = (eventName, handler, element = typeof window !== 'undefined' && window) => {
+  const savedHandler = useRef();
+
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(() => {
+    const isSupported = element && element.addEventListener;
+    if (!isSupported) return;
+
+    const eventListener = (event) => savedHandler.current(event);
+
+    element.addEventListener(eventName, eventListener);
+
+    return () => {
+      element.removeEventListener(eventName, eventListener);
+    };
+  }, [eventName, element]);
+};
